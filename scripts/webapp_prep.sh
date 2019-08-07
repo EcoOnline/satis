c310231ee7d909041f3a7d637b8667fc3e80bae1
Christian Gjengedal <cpg@basefarm.com>
2019-08-07 15:02:47 +0200

    composer cache moved to /local/app disk https://github.com/basefarm/ecoonline-iac/issues/13

--- scripts/webapp_prep.sh
+++ scripts/webapp_prep.sh
@@ -3,7 +3,11 @@
 add_prereqs(){
   amazon-linux-extras install php7.3
   yum install -y php-xml php-mbstring git
+
   wget -4 https://getcomposer.org/download/1.8.6/composer.phar -O /usr/local/bin/composer
+  mkdir -p /local/app/.composer/cache
+  php /usr/local/bin/composer config -g cache-dir "/local/app/.composer/cache"
+  php /usr/local/bin/composer config -g data-dir "/local/app/.composer"
 }
 
 add_satis(){
